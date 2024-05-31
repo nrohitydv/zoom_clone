bc08c3a3a0659776569dd7328f2e7a9e47a08830
Rohit <rohityadav787898@gmail.com>
2024-05-31 22:35:21 +0545

    "Added Clerk integration, updated globals.css, and modified layout.tsx and NavBar.tsx files"

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const NavBar = () => {
   return (
@@ -19,6 +20,9 @@ const NavBar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
